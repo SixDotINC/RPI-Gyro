592671feeb2c51de9e20fc547be2fa99a7d06453
gokbeykeskin <gokbeykeskin@gmail.com>
2022-06-01 00:21:51 +0300

    All files are uploaded.

---
+++ utils.py
@@ -0,0 +1,135 @@
+import os
+import sys
+from tempfile import gettempdir
+from subprocess import call
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import svm
+from scipy.signal import butter, lfilter, lfilter_zi
+
+
+NOTCH_B, NOTCH_A = butter(4, np.array([55, 65]) / (256 / 2), btype='bandstop')
+
+
+def epoch(data, samples_epoch, samples_overlap=0):
+
+    if isinstance(data, list):
+        data = np.array(data)
+
+    n_samples, n_channels = data.shape
+
+    samples_shift = samples_epoch - samples_overlap
+
+    n_epochs = int(
+        np.floor((n_samples - samples_epoch) / float(samples_shift)) + 1)
+
+    # Markers indicate where the epoch starts, and the epoch contains samples_epoch rows
+    markers = np.asarray(range(0, n_epochs + 1)) * samples_shift
+    markers = markers.astype(int)
+
+    # Divide data in epochs
+    epochs = np.zeros((samples_epoch, n_channels, n_epochs))
+
+    for i in range(0, n_epochs):
+        epochs[:, :, i] = data[markers[i]:markers[i] + samples_epoch, :]
+
+    return epochs
+
+
+def compute_band_powers(eegdata, fs):
+
+    # 1. Compute the PSD
+    winSampleLength, nbCh = eegdata.shape
+
+    # Apply Hamming window
+    w = np.hamming(winSampleLength)
+    dataWinCentered = eegdata - np.mean(eegdata, axis=0)  # Remove offset
+    dataWinCenteredHam = (dataWinCentered.T * w).T
+
+    NFFT = nextpow2(winSampleLength)
+    Y = np.fft.fft(dataWinCenteredHam, n=NFFT, axis=0) / winSampleLength
+    PSD = 2 * np.abs(Y[0:int(NFFT / 2), :])
+    f = fs / 2 * np.linspace(0, 1, int(NFFT / 2))
+
+    # SPECTRAL FEATURES
+    # Average of band powers
+    # Delta <4
+    ind_delta, = np.where(f < 4)
+    meanDelta = np.mean(PSD[ind_delta, :], axis=0)
+    # Theta 4-8
+    ind_theta, = np.where((f >= 4) & (f <= 8))
+    meanTheta = np.mean(PSD[ind_theta, :], axis=0)
+    # Alpha 8-12
+    ind_alpha, = np.where((f >= 8) & (f <= 12))
+    meanAlpha = np.mean(PSD[ind_alpha, :], axis=0)
+    # Beta 12-30
+    ind_beta, = np.where((f >= 12) & (f < 30))
+    meanBeta = np.mean(PSD[ind_beta, :], axis=0)
+
+    feature_vector = np.concatenate((meanDelta, meanTheta, meanAlpha,
+                                     meanBeta), axis=0)
+
+    feature_vector = np.log10(feature_vector)
+
+    return feature_vector
+
+
+def nextpow2(i):
+    n = 1
+    while n < i:
+        n *= 2
+    return n
+
+
+def compute_feature_matrix(epochs, fs):
+
+    n_epochs = epochs.shape[2]
+
+    for i_epoch in range(n_epochs):
+        if i_epoch == 0:
+            feat = compute_band_powers(epochs[:, :, i_epoch], fs).T
+            # Initialize feature_matrix
+            feature_matrix = np.zeros((n_epochs, feat.shape[0]))
+
+        feature_matrix[i_epoch, :] = compute_band_powers(
+            epochs[:, :, i_epoch], fs).T
+
+    return feature_matrix
+
+
+def get_feature_names(ch_names):
+
+    bands = ['delta', 'theta', 'alpha', 'beta']
+
+    feat_names = []
+    for band in bands:
+        for ch in range(len(ch_names)):
+            feat_names.append(band + '-' + ch_names[ch])
+
+    return feat_names
+
+
+def update_buffer(data_buffer, new_data, notch=False, filter_state=None):
+
+    if new_data.ndim == 1:
+        new_data = new_data.reshape(-1, data_buffer.shape[1])
+
+    if notch:
+        if filter_state is None:
+            filter_state = np.tile(lfilter_zi(NOTCH_B, NOTCH_A),
+                                   (data_buffer.shape[1], 1)).T
+        new_data, filter_state = lfilter(NOTCH_B, NOTCH_A, new_data, axis=0,
+                                         zi=filter_state)
+
+    new_buffer = np.concatenate((data_buffer, new_data), axis=0)
+    new_buffer = new_buffer[new_data.shape[0]:, :]
+
+    return new_buffer, filter_state
+
+
+def get_last_data(data_buffer, newest_samples):
+
+    new_buffer = data_buffer[(data_buffer.shape[0] - newest_samples):, :]
+
+    return new_buffer
